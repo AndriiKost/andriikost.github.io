41950b4cfe746743df2ad431c7b620466bc4841e
Andrii Kost <andriikost@Andriis-MacBook-Pro.local>
2018-04-10 15:15:05 -0500

    Added PAUSE button

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ startTime = (action) => {
           minutes: this.state.minutes + 1
         })
       } 
-      if (this.state.seconds <= 60) {
+      if (this.state.seconds < 60) {
        this.setState({seconds: this.state.seconds + 1})
       }
       if (this.state.minutes === 60) {
@@ -58,24 +58,38 @@ stopTime = () => {
   clearInterval(interval)
 }
 
+pauseTime = () => {
+  this.setState({
+    timer: false
+  })
+  clearInterval(interval)
+}
+
   render() {
-    const videoPlayer = <iframe title="Take a brake" width="420" height="345" src="http://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1" frameBorder="0" allowFullScreen></iframe>;
-    const motivationVideo = <iframe title="1 better than 0" width="560" height="315" src="https://www.youtube.com/embed/_Y_3euC4kxA?autoplay=1" frameBorder="0" allowFullScreen></iframe>;
+    const videoPlayer = <iframe title="Take a brake" width="320" height="245" src="http://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1" frameBorder="0" allowFullScreen></iframe>;
+    const motivationVideo = <iframe title="1 better than 0" width="320" height="245" src="https://www.youtube.com/embed/_Y_3euC4kxA?autoplay=1" frameBorder="0" allowFullScreen></iframe>;
     
+    const pause = "PAUSE"
+    const start = "START"
+
     return (
       <div className="App">
         <h2> Best Timer For Study </h2>
-        <p>It will help you to track time, concentrate and take a breaks</p>
+        <p>It will help you to track time, concentrate and take breaks</p>
         <div className="Timer">
           <span>{this.state.hours.toString().length === 1 ? '0' + this.state.hours : this.state.hours}:{this.state.minutes.toString().length === 1 ? '0' + this.state.minutes : this.state.minutes}:{this.state.seconds.toString().length === 1 ? '0' + this.state.seconds : this.state.seconds}</span>
         </div>
+        <div className="Buttons">
           <ul>
-            <li><button className="circle-btn" onClick={this.state.timer ? null : this.startTime}>START</button></li>
+            <li><button className="circle-btn" onClick={this.state.timer ? this.pauseTime : this.startTime}>{this.state.timer ? pause : start}</button></li>
             <li><button className="circle-btn" onClick={this.stopTime}>STOP</button></li>
           </ul>
-        {this.state.chill ? videoPlayer : null}
-        {this.state.motivation ? motivationVideo : null}
-      </div>
+        </div>
+          <div className="VideoBox">
+            {this.state.chill ? videoPlayer : null}
+            {this.state.motivation ? motivationVideo : null}
+          </div>
+        </div>
     );
   }
 }
